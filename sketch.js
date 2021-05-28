525bcc59cf15d0590f6d47781839c37e91c5782b
hooman_3311 <anshbuildtowin@gmail.com>
2021-05-28 14:52:26 +0530

    bird launched

--- sketch.js
+++ sketch.js
@@ -13,33 +13,33 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    createCanvas(windowWidth,windowHeight);
     engine = Engine.create();
     world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    ground = new Ground(600,height,width+500,20);
+    platform = new Ground(150, height-90, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(700,height-100,70,70);
+    box2 = new Box(920,height-100,70,70);
+    pig1 = new Pig(810, height-100);
+    log1 = new Log(810,height-120,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    box3 = new Box(670,height-140,70,70);
+    box4 = new Box(900,height-140,70,70);
+    pig3 = new Pig(810,height-140);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(810,height-160,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(810,height-170,70,70);
+    log4 = new Log(760,height-170,150, PI/7);
+    log5 = new Log(870,height-170,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,height-200);
 
     log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    slingShot = new Slingshot(bird.body,{x:200,y:height-325});
 }
 
 function draw(){
@@ -60,9 +60,20 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+        
+    slingShot.display();    
+}
+
+function mouseDragged()
+{
+
+Matter.Body.setPosition( bird.body,{x:mouseX,y:mouseY})
+
+}
+
+function mouseReleased()
+{
+    slingShot.fly()
 }
